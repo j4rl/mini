8a478c71cd4305923b11e3f41884d1bc0e855f95
j4rl <charlie.jarl@gmail.com>
2021-04-29 09:58:36 +0200

    Makin the shell

---
+++ func.php
@@ -0,0 +1,68 @@
+<?php
+
+//Connect to db
+function connect(){
+    $host="localhost";
+    $user="root";
+    $pass="";
+    $db="";
+    $conn=mysqli_connect($host,$user,$pass,$db) or die(mysqli_error);    
+    return $conn;
+}
+
+function runQuery($conntmp, $sqlStr){
+    if($result=mysql_query($conntmp,$sqlStr)){
+        return $result;
+    }else{
+        return false;
+    };
+
+}
+
+
+/* $host="localhost";
+$user="root";
+$pass="";
+$db="";
+$conn=mysqli_connect($host,$user,$pass,$db) or die(mysqli_error); */
+
+
+
+class db {
+    function __construct($db_name, $host="localhost", $username="root", $password="") {
+        $this->mysqli = mysqli_connect($host, $username, $password, $db_name);
+    }
+    public function fix($strFix){
+        return $this->mysqli->real_escape_string($strFix);
+    }
+
+    public function runQuery($strQuery){
+        $strTemp=$this->fix($strQuery);
+        return $this->mysqli->query($strQuery);
+    }
+
+    public function delRow($ID,$table){
+        $strTable=strtolower(substr($table,3,strlen($table)));
+        $tmpSTrID=$strTable."ID";
+        $query=$this->fix("DELETE FROM $table WHERE $tmpSTrID=$ID");
+        if($this->mysqli->query($query)){
+            return false;
+        }else{
+            return true;
+        };
+    }
+
+
+    public function getAll($table) //Returns an associated array
+    {
+        $query=$this->fix("SELECT * FROM $table");
+        return $this->mysqli->query($query)->fetch_assoc();
+    }
+
+    public function close(){
+        return $this->mysqli->close();
+    }
+
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<?php require_once('func.php'); ?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="content">
+    <form method="post" action="index.php" name="frmLogin" autocomplete="off">
+        <input type="text" name="username" placeholder="Användarnamn" required autocomplete="off">
+        <input type="password" onblur="this.setAttribute('readonly', 'readonly');" onfocus="this.removeAttribute('readonly');" readonly name="password" required autocomplete="off">
+        <button type="submit" name="login">Logga in</button>
+    </form>
+
+    <?php for($vdo=0;$vdo<3;$vdo++){ ?>
+        <section>
+            <h1>Lorem, ipsum.</h1>
+            <p class="ingress">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam porro consequuntur architecto ipsa a praesentium!</p>
+            <p class="text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente numquam aliquid vitae libero odio molestiae minus consequatur voluptas fugiat consectetur, similique alias minima obcaecati incidunt sunt dolorum fugit unde, veniam accusantium. Itaque eveniet voluptates dolorem fuga, quam quos possimus vel officiis impedit, tenetur nemo iusto alias dicta eos, voluptas obcaecati.</p>
+            <div class="byline">- by Charlie Jarl</div>
+        </section>
+<?php } ?>
+<?php for($vdo=0;$vdo<5;$vdo++){ ?>
+    <details>
+            <summary>Exempel</summary>
+            <div class="txtBox">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis quo fugiat vel.</div>
+            
+        </details>
+<?php } ?>
+
+    </div>
+</body>
+</html>
