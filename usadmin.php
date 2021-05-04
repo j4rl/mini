d55b737c8c2cd2a12e4d4170814608d2775ac0ab
Charlie Jarl <charlie.jarl@gmail.com>
2021-05-04 13:00:48 +0200

    ...Changes

--- usadmin.php
+++ usadmin.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="style.css">
     <script src="app.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js" integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
 </head>
 <body>
     
