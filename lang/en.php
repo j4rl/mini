b2d05830584948646636231b26a9a3169fbe19bb
Charlie Jarl <charlie.jarl@gmail.com>
2021-05-18 12:29:43 +0200

    lang support

---
+++ lang/en.php
@@ -0,0 +1,25 @@
+<?php
+
+//----------------------------------- Home Page
+$strHomeTitle="Mini blog";
+
+
+
+
+//-------------------------------- Menus
+$strHome="Home";
+$strAdmin="Admin";
+$strUserAdmin="Users";
+$strBlogAdmin="Blog";
+$strAbout="About";
+
+//------------------------------------ Blog Admin
+$placeholderHeader="Header";
+$strBtnNewBlog="Add blog!";
+$strEditBlog="Ok";
+
+
+
+
+
+?>
