58b97ba632de9f99acb5b86a38f5eb7670a31c95
Charlie Jarl <charlie.jarl@gmail.com>
2021-05-20 09:18:19 +0200

    SWE

---
+++ lang/sv.php
@@ -0,0 +1,47 @@
+<?php
+
+//----------------------------------- Home Page
+$strHomeTitle="Miniblogg";
+
+
+
+
+//-------------------------------- Menus
+$strHome="Hem";
+$strAdmin="Admin";
+$strUserAdmin="Användare";
+$strBlogAdmin="Blogg";
+$strAbout="Om";
+
+//------------------------------------ Blog Admin
+$placeholderHeader="Rubrik";
+$strBtnNewBlog="Lägg till";
+$strEditBlog="Ok";
+$strByLinePrefix="- av ";
+//------------------------------------ User admin
+$strBtnEditUser="Ok";
+$strBtnNewUser="Lägg till!";
+$strAddUser="Lägg till användare";
+$placeholderRealname="Namn";
+$placeholderUsername="Användarnamn";
+$placeholderPassword="Lösenord";
+$strUserlevel="Behörighet:";
+//-------------------------------- funcfile
+$strMessLoginFail="Kunde inte logga in.";
+$strMessLoginSuccess="Inloggad.";
+$strMessUnknown="Okänt fel!";
+//------------------------------------
+$strMessWrongID="Fel ID!";
+$strMessUsernameFail="Hittade inte användarnamn.";
+
+
+$strLogout="Logga ut";
+$strLogin="Logga in";
+$strMessDoLogin="Inte inloggad, logga in.";
+
+$strAdmin="Admin";
+$strExtended="Utökad användare";
+$strUser="Normal användare";
+$strErrUser="Din behörighet är inte giltig!";
+
+?>
